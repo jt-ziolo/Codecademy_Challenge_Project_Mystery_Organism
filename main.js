d35fd4600f64f3bfa9d7196139692fcc9ea9d6db
JT Ziolo <ziolojt@gmail.com>
2023-04-04 22:01:28 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+function pAequorFactory(specimenNum, dna) {
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    mutate() {
+    },
+  };
+}
+
+console.log(returnRandBase());
+console.log(mockUpStrand());
+
+console.log(pAequorFactory(1, ['A', 'B']));
+
+
+
+
+
+
